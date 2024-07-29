5665e32c43be5b05055aaaaec88606235a5da7b3
Phelipe Magalhães <phelipemagalhaes13@gmail.com>
2024-07-29 20:44:59 -0300

    Alter return end point to ResponseEntity
    Add good practices

--- src/main/java/med/voll/api/controller/MedicController.java
+++ src/main/java/med/voll/api/controller/MedicController.java
@@ -8,7 +8,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -21,26 +23,42 @@ public class MedicController {
 
     @PostMapping
     @Transactional
-    public void register(@RequestBody @Valid RegisterMedicDTO medicDTO) {
-        repository.save(new Medic(medicDTO));
+    public ResponseEntity register(@RequestBody @Valid RegisterMedicDTO medicDTO, UriComponentsBuilder uriComponentsBuilder) {
+        var medic = new Medic(medicDTO);
+        repository.save(medic);
+
+        var uri = uriComponentsBuilder.path("/medic/{id}").buildAndExpand(medic.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DetailMedicDTO(medic));
     }
 
     @GetMapping
-    public Page<ListMedicDTO> list(@PageableDefault(size = 2, sort = {"name"}, direction = Sort.Direction.DESC) Pageable pageable) {
-        return repository.findAllByActiveTrue(pageable).map(ListMedicDTO::new);
+    public ResponseEntity<Page<ListMedicDTO>> list(@PageableDefault(size = 5, sort = {"name"}, direction = Sort.Direction.DESC) Pageable pageable) {
+        var page = repository.findAllByActiveTrue(pageable).map(ListMedicDTO::new);
+        return ResponseEntity.ok(page);
     }
 
     @PutMapping
     @Transactional
-    public void update(@RequestBody @Valid UpdateMedicDTO medicDTO) {
+    public ResponseEntity update(@RequestBody @Valid UpdateMedicDTO medicDTO) {
         var medic = repository.getReferenceById(medicDTO.id());
         medic.updateData(medicDTO);
+
+        return ResponseEntity.ok(new DetailMedicDTO(medic));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void delete(@PathVariable Long id) {
+    public ResponseEntity delete(@PathVariable Long id) {
         var medic = repository.getReferenceById(id);
         medic.delete();
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detail(@PathVariable Long id) {
+        var medic = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DetailMedicDTO(medic));
     }
 }
---
+++ src/main/java/med/voll/api/medic/DetailMedicDTO.java
@@ -0,0 +1,9 @@
+package med.voll.api.medic;
+
+public record DetailMedicDTO(Long id, String name, String email, String crm, String phone, Specialty specialty,
+                             Address address) {
+
+    public DetailMedicDTO(Medic medic) {
+        this(medic.getId(), medic.getName(), medic.getEmail(), medic.getCrm(), medic.getPhone(), medic.getSpecialty(), medic.getAddress());
+    }
+}
