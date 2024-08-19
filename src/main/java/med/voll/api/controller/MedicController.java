b350a26c45bfe8b80d88394c74dbf4b10da637a6
Phelipe Magalhães <phelipemagalhaes13@gmail.com>
2024-08-19 19:25:44 -0300

    add spring doc (swagger)

--- src/main/java/med/voll/api/controller/MedicController.java
+++ src/main/java/med/voll/api/controller/MedicController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.domain.medic.ListMedicDTO;
@@ -17,6 +18,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("medics")
+@SecurityRequirement(name = "bearer-key")
 public class MedicController {
 
     @Autowired
--- src/main/java/med/voll/api/controller/PatientController.java
+++ src/main/java/med/voll/api/controller/PatientController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.domain.DetailPatientDTO;
@@ -19,6 +20,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("patients")
+@SecurityRequirement(name = "bearer-key")
 public class PatientController {
     @Autowired
     private PatientRepository repository;
